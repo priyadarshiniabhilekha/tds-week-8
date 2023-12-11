289c2409dded6b37790f2c10f6cf86c488be4370
priyadarshiniabhilekha <146136933+priyadarshiniabhilekha@users.noreply.github.com>
2023-12-11 20:56:12 +0530

    Update abhi.py

--- abhi.py
+++ abhi.py
@@ -1,7 +1,18 @@
 import streamlit as st
+from pattern_generator import generate_pattern
 
-st.title(" Abhilekha Priyadarshini ")
+st.title("Pattern Generator with Streamlit")
 
-n = input()
-for i in range(1,len(n)+1):
-    print(n[:i])
+# Streamlit input
+user_input = st.text_input("Enter a string:")
+result = generate_pattern(user_input)
+
+# Display the output in Streamlit
+st.text("Generated Pattern:")
+for line in result:
+    st.text(line)
+
+
+def generate_pattern(input_str):
+    for i in range(1,len(n)+1):
+        return n[:i]
