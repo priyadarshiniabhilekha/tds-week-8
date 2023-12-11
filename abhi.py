fdb0abedcea0a94e53998fd9bef78d063d4de0a4
priyadarshiniabhilekha <146136933+priyadarshiniabhilekha@users.noreply.github.com>
2023-12-11 20:57:06 +0530

    Update abhi.py

--- abhi.py
+++ abhi.py
@@ -1,3 +1,19 @@
+def generate_pattern(input_str):
+    length = len(input_str)
+    pattern_list = []
+
+    for i in range(1, length + 1):
+        pattern_list.append(input_str[:i])
+
+    return pattern_list
+
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
+    result = generate_pattern(user_input)
+    
+    for line in result:
+        print(line)
+
 import streamlit as st
 from pattern_generator import generate_pattern
 
@@ -12,7 +28,3 @@ st.text("Generated Pattern:")
 for line in result:
     st.text(line)
 
-
-def generate_pattern(input_str):
-    for i in range(1,len(n)+1):
-        return n[:i]
